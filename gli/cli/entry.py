8a0ee2d416772a459be9f55bf2c384b6485b8fa9
Audric.S <evinoca@gmail.com>
2020-05-25 18:43:46 +0800

    update cmd prefix

--- gli/cli/entry.py
+++ gli/cli/entry.py
@@ -27,21 +27,21 @@ class Environment:
 
 
 pass_environment = click.make_pass_decorator(Environment, ensure=True)
-cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "subs"))
 
 
 class ComplexCLI(click.MultiCommand):
     def list_commands(self, ctx):
         rv = []
         for filename in os.listdir(cmd_folder):
-            if filename.endswith(".py") and filename.startswith("cmd_"):
+            if filename.endswith(".py") and filename.startswith("c_"):
                 rv.append(filename[4:-3])
         rv.sort()
         return rv
 
     def get_command(self, ctx, name):
         try:
-            mod = __import__(f"gli.cli.commands.cmd_{name}", None, None, ["cli"])
+            mod = __import__(f"gli.cli.subs.c_{name}", None, None, ["cli"])
         except ImportError:
             return
         return mod.cli
